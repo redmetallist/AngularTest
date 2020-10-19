5e41efad2115b8a38895d559bd1c729804dc52f1
Mikalai <red_metallist@mail.ru>
2020-10-19 23:43:16 +0300

    save current stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {timeout} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulartest';
+  number = 42;
+  arr = [1,2,3,4]
+  obj = {
+    a: 1, b: {c: 2}
+  }
+
+  inputValue = ''
+
+
+  // img = 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
+
+  constructor() {
+    // setTimeout( () => {
+    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg'
+    // },  5000);
+  }
+
+  onInput(event?){
+    console.log('event', event)
+  }
+
 }
