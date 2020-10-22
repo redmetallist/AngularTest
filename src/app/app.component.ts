d2e912af5c8311309c4c73c9536f008635605577
Mikalai <red_metallist@mail.ru>
2020-10-22 22:29:54 +0300

    commit from 22.10.20. Going to next level.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,6 @@ import {timeout} from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angulartest';
-  number = 42;
-  arr = [1,2,3,4]
-  obj = {
-    a: 1, b: {c: 2}
-  }
 
-  inputValue = ''
-
-
-  // img = 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
-
-  constructor() {
-    // setTimeout( () => {
-    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg'
-    // },  5000);
-  }
-
-  onInput(event?){
-    console.log('event', event)
-  }
-
+  now : Date = new Date() ;
 }
