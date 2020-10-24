d3c3ece33c6c3f09b3086a9ccfbf062b66e3425a
Mikalai <red_metallist@mail.ru>
2020-10-24 23:01:25 +0300

    going to block4

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import { Post } from '../app.component';
 
 @Component({
   selector: 'app-post-form',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostFormComponent implements OnInit {
 
+  @Output() onAdd: EventEmitter<Post> = new EventEmitter<Post>();
+
+  @ViewChild('titleInput') inputRef : ElementRef ;
+
+  title : string = '';
+  text : string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addPost()
+  {
+    if(this.text.trim() && this.title.trim())
+    {
+      const post: Post = {
+        title: this.title,
+        text: this.text
+      }
+      this.onAdd.emit(post) ;
+      console.log('new post ', post) ;
+      this.title = this.text = '' ;
+    }
+
+  }
+
+
+  focusTitle() {
+this.inputRef.nativeElement.focus();
+  }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
+import {Post} from '../app.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  changeDetection : ChangeDetectionStrategy.OnPush,
+  encapsulation : ViewEncapsulation.None
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit ,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,OnDestroy
+{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() post: Post ;
+@ContentChild('info', {static : true}) infoRef : ElementRef ;
+@Output() onRemove = new EventEmitter<number>() ;
+  constructor() {
+    console.log('constructor') ;
   }
 
+  ngOnInit(): void {
+    console.log('ngOnInit') ;
+  //  console.log(this.infoRef.nativeElement) ;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges', changes) ;
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck') ;
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+
+  removePost() {
+this.onRemove.emit(this.post.id) ;
+  }
+
+  ngOnDestroy(): void {
+    console.log('onDestroy')
+  }
 }
