a737c5189b915574d8b8ea801a3c63a69e81a76a
Mikalai <red_metallist@mail.ru>
2020-10-26 20:03:26 +0300

    cool pipe whitch can filtrate search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {timeout} from 'rxjs/operators';
 
+export interface Post {
+  title: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,11 @@ import {timeout} from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-e:number = Math.E ;
-str:string = 'hello world'
+posts: Post [] = [
+  {    title: 'beer', text: 'most tasty beer in the world' },
+  {    title: 'bread', text: 'most tasty bread in the world' },
+  {    title: 'JS', text: 'fucking js' }
+]
+  search = '';
+searchField = 'title'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
